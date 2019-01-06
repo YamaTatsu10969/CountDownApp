3920487aa461167899cf0e4c9a908670e7ad0c7d
yamamoto tatsuya <52.wadachi.96@gmail.com>
2019-01-06 16:13:01 +0900

    feat:calc and show result

--- CountDownApp/CountDownApp/ViewController.swift
+++ CountDownApp/CountDownApp/ViewController.swift
@@ -9,12 +9,55 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var CountLabel: UILabel!
+    
+    var count:Double = 180.00
+    var timer = Foundation.Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.CountLabel.text = "03:00:00"
     }
+    
+    
+    @IBAction func didTouchedSettingButton(_ sender: Any) {
+//        guard let input = CountLabel.text else {
+//            return
+//        }
+//        guard let changeDouble = Double(input) else{
+//            return
+//        }
+//        self.count = changeDouble
+    }
+    
+    @IBAction func didTouchedPlayButton(_ sender: Any) {
+        
+        timer = Foundation.Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(self.update), userInfo: nil, repeats: true)
+        
+    }
+    
+    @objc func update(){
+        count -= 0.01
+        calcAndShowResult()
 
-
+    }
+    
+    func calcAndShowResult(){
+        let t = count
+        let min = Int(t/60)
+        let sec = Int(t)%60
+        let msec =  Int(count * 100) - (Int(count) * 100)
+        self.CountLabel.text = String(format:"%02d:%02d:%02d", min, sec, msec)
+    }
+    
+    @IBAction func didTouchedPauseButton(_ sender: Any) {
+        self.timer.invalidate()
+    }
+    
+    @IBAction func didTouchedResetButton(_ sender: Any) {
+        count = 0.00
+        calcAndShowResult()
+    }
 }
 
